1b0bbb8eafe008c3d51ea07eff9d17225ebc2f40
MaksKuchuk <94689394+MaksKuchuk@users.noreply.github.com>
2022-01-01 21:29:42 +1000

    1

---
+++ app/src/main/java/com/example/dvfuyaimp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.dvfuyaimp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.enterButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                enterBTN(view);
+            }
+        });
+        findViewById(R.id.restorePasswordButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                restorePasswordBTN(view);
+            }
+        });
+
+    }
+
+    public void enterBTN(View view){
+        Toast.makeText(getApplicationContext(),"Enter", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(this, MainScreen.class));
+    }
+
+    public void restorePasswordBTN(View view){
+        Toast.makeText(getApplicationContext(),"Restore password", Toast.LENGTH_SHORT).show();
+    }
+
+
+}
