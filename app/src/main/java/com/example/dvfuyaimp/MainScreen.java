f62a32105e6498165010e7eaf906e11b4c6c9a29
MrGriboman <96828637+MrGriboman@users.noreply.github.com>
2022-01-03 14:20:14 +1000

    settingsActivity(draft)

--- app/src/main/java/com/example/dvfuyaimp/MainScreen.java
+++ app/src/main/java/com/example/dvfuyaimp/MainScreen.java
@@ -2,6 +2,7 @@ package com.example.dvfuyaimp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -90,6 +91,7 @@ public class MainScreen extends AppCompatActivity {
 
     public void settingBTN(View view){
         Toast.makeText(getApplicationContext(),"Settings", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(this, SettingsActivity.class));
     }
     public void calendarBTN(View view){
         Toast.makeText(getApplicationContext(),"Calendar", Toast.LENGTH_SHORT).show();
