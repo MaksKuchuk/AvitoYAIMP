878b0d666aa472c460c0410d18b991ed1d15d24a
olimp <bolotaev.rala@students.dvfu.ru>
2022-01-04 06:02:22 +1000

    Remove Login and Password property

--- Server/Schedule/Profile.cs
+++ Server/Schedule/Profile.cs
@@ -3,21 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Schedule
 {
     public class Profile
     {
-        public Profile(string login, string password, string firstname, string lastname, string acctype)
+        public Profile(string firstname, string lastname, string acctype)
         {
-            Login = login;
-            Password = password;
             FirstName = firstname;
             LastName = lastname;
             AccType = acctype;
         }
-        public string Login { get; set; }
-        public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string AccType { get; set; }
