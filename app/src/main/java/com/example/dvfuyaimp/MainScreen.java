ed30c73aeb5b837c638c0855c7ff006f578cadd0
MaksKuchuk <94689394+MaksKuchuk@users.noreply.github.com>
2022-01-05 01:41:45 +1000

    addLesson addEvent

--- app/src/main/java/com/example/dvfuyaimp/MainScreen.java
+++ app/src/main/java/com/example/dvfuyaimp/MainScreen.java
@@ -6,9 +6,12 @@ import android.app.ActivityOptions;
 import android.content.Intent;
 import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainScreen extends AppCompatActivity {
@@ -73,6 +76,9 @@ public class MainScreen extends AppCompatActivity {
         });
 
         setStartDay(findViewById(R.id.MonBTN));
+        addLesson("YAIMP", "Sush", "15:10", "D820");
+        addLesson("VAISD", "Klenin", "11:50", "G464");
+        addEvent("contest", "15:10", "Building A");
     }
 
     public void MonBTN(View view){
@@ -134,5 +140,28 @@ public class MainScreen extends AppCompatActivity {
     private void setStartDay(View view){
         LastWeekBTN = findViewById(R.id.MonBTN);
         ((TransitionDrawable)view.getBackground()).startTransition(0);
+    }
+
+    private void addLesson(String strLesson, String strTeacher, String strTime, String strRoom){
+        LayoutInflater ltInflater = getLayoutInflater();
+        View item = ltInflater.inflate(R.layout.lesson_layout, ((LinearLayout)findViewById(R.id.subjectScrollLayout)), false);
+
+        ((TextView)item.findViewById(R.id.lesson)).setText(strLesson);
+        ((TextView)item.findViewById(R.id.teacher)).setText(strTeacher);
+        ((TextView)item.findViewById(R.id.time)).setText(strTime);
+        ((TextView)item.findViewById(R.id.room)).setText(strRoom);
+
+        ((LinearLayout)findViewById(R.id.subjectScrollLayout)).addView(item);
+    }
+
+    private void addEvent(String strEvent, String strTimeEvent, String strPlace){
+        LayoutInflater ltInflater = getLayoutInflater();
+        View item = ltInflater.inflate(R.layout.event_layout, ((LinearLayout)findViewById(R.id.subjectScrollLayout)), false);
+
+        ((TextView)item.findViewById(R.id.event)).setText(strEvent);
+        ((TextView)item.findViewById(R.id.timeEvent)).setText(strTimeEvent);
+        ((TextView)item.findViewById(R.id.place)).setText(strPlace);
+
+        ((LinearLayout)findViewById(R.id.subjectScrollLayout)).addView(item);
     }
 }
